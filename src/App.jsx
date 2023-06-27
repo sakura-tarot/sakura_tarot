d31eec6bcface3930bd717ab65ae9af7811b3d5c
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-06-27 12:33:05 +0200

    Creación header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import HomePage from './pages/Home/HomePage'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <HomePage/>
     </>
   )
 }
